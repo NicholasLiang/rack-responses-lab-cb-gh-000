59766f290bfb9070c1b4cf2e2134c3cbb285cf1c
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:36:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     
     current_time = Time.now
     
+<<<<<<< HEAD
     # resp.write "#{current_time}"   # response : 2018-05-02 22:23:20 +0000
     
     # resp.write "#{current_time.hour}"   # response : 22
@@ -15,6 +16,9 @@ class Application
     else
       resp.write "Good Afternoon!"
     end 
+=======
+    resp.write "#{current_time}"
+>>>>>>> d3352683c924ef345f2c36b8f587ee91ffaa3069
     
     resp.finish
   end
